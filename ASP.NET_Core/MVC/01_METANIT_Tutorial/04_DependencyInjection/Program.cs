f98287e5bb513d3a49afc923b8a0d27ada4c9e92
Vik154 <Smorodin_1209@mail.ru>
2023-11-03 21:09:07 +0700

    Внедрение зависимостей и IServiceCollection

---
+++ ASP.NET_Core/MVC/01_METANIT_Tutorial/04_DependencyInjection/Program.cs
@@ -0,0 +1,12 @@
+namespace _04_DependencyInjection {
+    public class Program {
+        public static void Main(string[] args) {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
