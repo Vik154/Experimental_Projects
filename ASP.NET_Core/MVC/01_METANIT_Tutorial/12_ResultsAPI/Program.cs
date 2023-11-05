db89728d8719f92db672080a2350fd58f22849c7
Vik154 <Smorodin_1209@mail.ru>
2023-11-05 22:38:01 +0700

    Введение в Results API

--- ASP.NET_Core/MVC/01_METANIT_Tutorial/12_ResultsAPI/Program.cs
+++ ASP.NET_Core/MVC/01_METANIT_Tutorial/12_ResultsAPI/Program.cs
@@ -1,12 +1,23 @@
 ﻿namespace _12_ResultsAPI;
 
-
 public class Program {
     public static void Main(string[] args) {
         var builder = WebApplication.CreateBuilder(args);
         var app = builder.Build();
 
-        app.MapGet("/", () => "Hello World!");
+        /// <summary>
+        /// Обе конечные точки продуцируют почти аналогичный ответ: 
+        /// также будет отправляться одна и та же строка со статусным кодом 200.
+        /// Разница будет заключаться только в отдельных заголовках 
+        /// (в частности, метод Results.Text добавляет заголовок "Content-Length").
+        /// </summary>
+        app.Map("/hello", () => Results.Text("Hello ASP.NET Core"));
+        app.Map("/", () => "Hello ASP.NET Core");
+
+        /// <summary> Results API (IResult) отправка ответа в обычных компонентах middleware</summary>
+        app.Map("/IResult", async context => {
+            await Results.Text("Hello ASP.NET Core").ExecuteAsync(context);
+        });
 
         app.Run();
     }
