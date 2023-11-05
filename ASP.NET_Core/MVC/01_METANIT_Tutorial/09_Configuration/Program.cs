cfd9cdbf989ac3a0c8fd81dc324f820c6c726ebb
Vik154 <Smorodin_1209@mail.ru>
2023-11-05 18:45:38 +0700

    Работа с конфигурацией

---
+++ ASP.NET_Core/MVC/01_METANIT_Tutorial/09_Configuration/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _09_Configuration;
+
+
+public class Program {
+    public static void Main(string[] args) {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.MapGet("/", () => "Hello World!");
+
+        app.Run();
+    }
+}
