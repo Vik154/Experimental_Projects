e0a3571591f49062221d5bc82816c7bbb76f1c4d
Vik154 <Smorodin_1209@mail.ru>
2023-11-05 20:04:11 +0700

    Логирование

--- ASP.NET_Core/MVC/01_METANIT_Tutorial/11_Logger/Program.cs
+++ ASP.NET_Core/MVC/01_METANIT_Tutorial/11_Logger/Program.cs
@@ -6,7 +6,28 @@ public class Program {
         var builder = WebApplication.CreateBuilder(args);
         var app = builder.Build();
 
-        app.MapGet("/", () => "Hello World!");
+
+        app.Map("/", async (context) => {
+            // пишем на консоль информацию
+            app.Logger.LogInformation($"Processing request {context.Request.Path}");
+            await context.Response.WriteAsync("Hello World!");
+        });
+
+        /// <summary> Получение логгера через внедрение зависимостей </ summary>
+        app.Map("/hello", (ILogger<Program> logger) => {
+            logger.LogInformation($"Path: /hello  Time: {DateTime.Now.ToLongTimeString()}");
+            return "Hello World";
+        });
+
+        app.Map("/log", async (context) => {
+            var path = context.Request.Path;
+            app.Logger.LogCritical($"LogCritical {path}");
+            app.Logger.LogError($"LogError {path}");
+            app.Logger.LogInformation($"LogInformation {path}");
+            app.Logger.LogWarning($"LogWarning {path}");
+
+            await context.Response.WriteAsync("Hello World!");
+        });
 
         app.Run();
     }
