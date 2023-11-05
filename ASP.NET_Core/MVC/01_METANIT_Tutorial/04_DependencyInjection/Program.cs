463ad60cb40485941cd91b58c18cd7eeebabe738
Vik154 <Smorodin_1209@mail.ru>
2023-11-05 11:14:58 +0700

    Принт всех встроенных сервисов

--- ASP.NET_Core/MVC/01_METANIT_Tutorial/04_DependencyInjection/Program.cs
+++ ASP.NET_Core/MVC/01_METANIT_Tutorial/04_DependencyInjection/Program.cs
@@ -1,12 +1,44 @@
-namespace _04_DependencyInjection {
-    public class Program {
-        public static void Main(string[] args) {
-            var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
+using System.Text;
 
-            app.MapGet("/", () => "Hello World!");
+namespace _04_DependencyInjection;
 
-            app.Run();
-        }
+
+public class Program {
+
+    /// <summary> IServiceCollection - все встроенные сервисы </summary>
+    static IServiceCollection Services { get; set; }
+
+    public static void Main(string[] args) {
+        var builder = WebApplication.CreateBuilder(args);
+
+        /// <summary> Получение всех встроенных сервисов </summary>
+        Services = builder.Services;    
+        var app = builder.Build();
+
+        app.Run(TestServices);
+        app.Run();
+
     }
+
+    #region ВСТРОЕННЫЕ СЕРВИСЫ IServiceCollection
+
+    /// <summary> Вывод всех встроенных сервисов фреймворка </summary>
+    static async Task TestServices(HttpContext context) {
+        var sb = new StringBuilder();
+        sb.Append("<h1>Все сервисы</h1>");
+        sb.Append("<table>");
+        sb.Append("<tr><th>Тип</th><th>Lifetime</th><th>Реализация</th></tr>");
+        foreach (var svc in Services) {
+            sb.Append("<tr>");
+            sb.Append($"<td>{svc.ServiceType.FullName}</td>");
+            sb.Append($"<td>{svc.Lifetime}</td>");
+            sb.Append($"<td>{svc.ImplementationType?.FullName}</td>");
+            sb.Append("</tr>");
+        }
+        sb.Append("</table>");
+        context.Response.ContentType = "text/html;charset=utf-8";
+        await context.Response.WriteAsync(sb.ToString());
+    }
+
+    #endregion
 }
