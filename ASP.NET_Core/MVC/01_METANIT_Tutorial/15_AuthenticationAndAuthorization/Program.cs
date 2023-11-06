b71a8f95ff775622b46e1dbc8a297988cab32615
Vik154 <Smorodin_1209@mail.ru>
2023-11-06 19:30:48 +0700

    аутентификации и авторизации

--- ASP.NET_Core/MVC/01_METANIT_Tutorial/15_AuthenticationAndAuthorization/Program.cs
+++ ASP.NET_Core/MVC/01_METANIT_Tutorial/15_AuthenticationAndAuthorization/Program.cs
@@ -1,13 +1,24 @@
-﻿namespace _15_AuthenticationAndAuthorization;
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace _15_AuthenticationAndAuthorization;
 
 
 public class Program {
     public static void Main(string[] args) {
         var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddAuthentication("Bearer")    // добавление сервисов аутентификации
+            .AddJwtBearer();                            // подключение аутентификации с помощью jwt-токенов
+        builder.Services.AddAuthorization();            // добавление сервисов авторизации
+
         var app = builder.Build();
 
-        app.MapGet("/", () => "Hello World!");
+        app.UseAuthentication();    // добавление middleware аутентификации 
+        app.UseAuthorization();     // добавление middleware авторизации 
 
+        app.Map("/hello", [Authorize] () => "Hello World!");
+        app.Map("/", () => "Home Page");
+        
         app.Run();
     }
 }
