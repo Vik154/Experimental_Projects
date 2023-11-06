0101455707833038ebc62bd10e74ab2bc1ccd652
Vik154 <Smorodin_1209@mail.ru>
2023-11-06 19:51:34 +0700

    Авторизация с помощью JWT-токенов в клиенте JavaScript

---
+++ ASP.NET_Core/MVC/01_METANIT_Tutorial/16_AuthorizationJWT/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _16_AuthorizationJWT;
+
+
+public class Program {
+    public static void Main(string[] args) {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.MapGet("/", () => "Hello World!");
+
+        app.Run();
+    }
+}
