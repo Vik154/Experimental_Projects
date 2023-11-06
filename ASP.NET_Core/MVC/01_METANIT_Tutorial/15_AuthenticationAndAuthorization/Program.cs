bbc5fb531d349f70d43bbef5cc4d0825fe28c1c6
Vik154 <Smorodin_1209@mail.ru>
2023-11-06 18:37:58 +0700

    Аутентификация и авторизация

---
+++ ASP.NET_Core/MVC/01_METANIT_Tutorial/15_AuthenticationAndAuthorization/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _15_AuthenticationAndAuthorization;
+
+
+public class Program {
+    public static void Main(string[] args) {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.MapGet("/", () => "Hello World!");
+
+        app.Run();
+    }
+}
