4bbc6d920eaf4a5026861adbd28dec800267aa1e
Vik154 <Smorodin_1209@mail.ru>
2023-11-11 16:41:46 +0700

    Получение параметров маршрутов в контроллере

---
+++ ASP.NET_Core/MVC/02_METANIT_MVCTutorial/03_Routes/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _03_Routes.Controllers;
+
+public class HomeController : Controller {
+
+    /// <summary> Получение параметров маршрутов в контроллере </summary>
+    public string Index() {
+        var controller = RouteData.Values["controller"];
+        var action = RouteData.Values["action"];
+        return $"controller: {controller} | action: {action}";
+    }
+}
