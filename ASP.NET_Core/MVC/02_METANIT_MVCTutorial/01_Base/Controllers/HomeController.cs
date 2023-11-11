11e485b7e08dcdb17d4f7b89c58f41fc1a6dcea9
Vik154 <Smorodin_1209@mail.ru>
2023-11-11 12:50:29 +0700

    Контекст контроллера

---
+++ ASP.NET_Core/MVC/02_METANIT_MVCTutorial/01_Base/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _01_Base.Controllers;
+
+
+public class HomeController : Controller {
+
+    public string Index() => "Index View";
+
+    public async Task Info() {
+        Response.ContentType = "text/html;charset=utf-8";
+        await Response.WriteAsync("<h2>Hello World</h2>");
+    }
+
+    public async Task HeaderInfo() {
+        Response.ContentType = "text/html;charset=utf-8";
+        System.Text.StringBuilder tableBuilder = new("<h2>Request headers</h2><table>");
+        foreach (var header in Request.Headers) {
+            tableBuilder.Append($"<tr><td>{header.Key}</td><td>{header.Value}</td></tr>");
+        }
+        tableBuilder.Append("</table>");
+        await Response.WriteAsync(tableBuilder.ToString());
+    }
+
+    [HttpGet]
+    public string Get() => "Get";
+}
--- ASP.NET_Core/MVC/02_METANIT_MVCTutorial/01_Base/Program.cs
+++ ASP.NET_Core/MVC/02_METANIT_MVCTutorial/01_Base/Program.cs
@@ -3,9 +3,14 @@
 public class Program {
     public static void Main(string[] args) {
         var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddControllersWithViews();
+
         var app = builder.Build();
 
-        app.MapGet("/", () => "Hello World!");
+        app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Info}/{id?}");
 
         app.Run();
     }
