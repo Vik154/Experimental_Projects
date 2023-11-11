e8c6a34f94c5969dc2984c0664280528d5cd5a50
Vik154 <Smorodin_1209@mail.ru>
2023-11-11 19:37:11 +0700

    Передача параметров в View Component

---
+++ ASP.NET_Core/MVC/02_METANIT_MVCTutorial/07_ViewComponent/Components/Stopwatch.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _07_ViewComponent.Components;
+
+[ViewComponent]
+public class Stopwatch {
+    public string Invoke(bool includeSeconds, bool format24) {
+        string time;
+        DateTime now = DateTime.Now;
+
+        if (format24)   // если 24-часовой формат
+            time = now.ToString("HH:mm");
+        else           // если 12-часовой формат
+            time = now.ToString("hh:mm");
+
+        if (includeSeconds) // если надо добавить секунды
+            time = $"{time}:{now.Second}";
+
+        return $"Текущее время: {time}";
+    }
+}
