56fc1fe3c11dc77d7790c4af9803cf0a6b14f2e7
Vik154 <Smorodin_1209@mail.ru>
2023-11-11 17:56:41 +0700

    HTML-хелперы элементов форм

---
+++ ASP.NET_Core/MVC/02_METANIT_MVCTutorial/05_HtmlHelpers/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _05_HtmlHelpers.Controllers;
+
+public class HomeController : Controller {
+    public IActionResult Index() => View();
+
+    [HttpGet]
+    public IActionResult Create() => View();
+
+    [HttpPost]
+    public string Create(string name, int age) => $"{name} - {age}";
+}
--- ASP.NET_Core/MVC/02_METANIT_MVCTutorial/05_HtmlHelpers/Program.cs
+++ ASP.NET_Core/MVC/02_METANIT_MVCTutorial/05_HtmlHelpers/Program.cs
@@ -3,9 +3,12 @@
 public class Program {
     public static void Main(string[] args) {
         var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddMvc();
+
         var app = builder.Build();
 
-        app.MapGet("/", () => "Hello World!");
+        app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
 
         app.Run();
     }
