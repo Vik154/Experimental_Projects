81e5d9e425a1191013904d513ae161c627eee497
Vik154 <Smorodin_1209@mail.ru>
2023-11-11 16:07:23 +0700

    Работа с формами

---
+++ ASP.NET_Core/MVC/02_METANIT_MVCTutorial/02_Views/Controllers/FormController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _02_Views.Controllers;
+
+/// <summary> Работа с формами </summary>
+public class FormController : Controller {
+
+    [HttpGet]
+    public IActionResult Index() => View();
+    
+    [HttpPost]
+    public string Index(string username, string password, int age, string comment) {
+        return $"User Name: {username}   Password: {password}   Age: {age}  Comment: {comment}";
+    }
+}
