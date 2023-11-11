866fdc1bf03f521f8932a1faf35a8d96bd3ce569
Vik154 <Smorodin_1209@mail.ru>
2023-11-11 15:26:48 +0700

    Передача данных в представление

--- ASP.NET_Core/MVC/02_METANIT_MVCTutorial/02_Views/Controllers/HomeController.cs
+++ ASP.NET_Core/MVC/02_METANIT_MVCTutorial/02_Views/Controllers/HomeController.cs
@@ -7,6 +7,8 @@ public class HomeController : Controller {
     /// <summary> View(): для генерации ответа используется представление,
     /// которое по имени совпадает с вызывающим методом, если явно не указано иное</summary>
     public IActionResult Index() {
+        ViewData["Message"] = "Hello from ViewData";
+        ViewBag.Title = "Hello from ViewBag";
         return View();
     }
 
