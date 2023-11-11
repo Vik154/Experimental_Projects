bc5d33dc70fea09a561711b5abda0ce12e9ecaeb
Vik154 <Smorodin_1209@mail.ru>
2023-11-11 13:47:45 +0700

    Результаты действий

---
+++ ASP.NET_Core/MVC/02_METANIT_MVCTutorial/01_Base/Controllers/HtmlController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _01_Base.Controllers;
+
+public class HtmlController : Controller {
+    public IActionResult Index() {
+        return new HtmlResult("Hello world");
+    }
+}
---
+++ ASP.NET_Core/MVC/02_METANIT_MVCTutorial/01_Base/HtmlResult.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _01_Base;
+
+/// <summary> Собственный класс результата действий </summary>
+public class HtmlResult : IActionResult {
+    string htmlCode;
+    public HtmlResult(string html) => htmlCode = html;
+    public async Task ExecuteResultAsync(ActionContext context) {
+        string fullHtmlCode = @$"<!DOCTYPE html>
+            <html>
+                <head>
+                    <title>METANIT.COM</title>
+                    <meta charset=utf-8 />
+                </head>
+                <body>{htmlCode}</body>
+            </html>";
+        await context.HttpContext.Response.WriteAsync(fullHtmlCode);
+    }
+}
