0df4a984a8484f640f66a7be5748b291659c60a4
Vik154 <Smorodin_1209@mail.ru>
2023-11-11 19:42:58 +0700

    ViewComponentResult и генерация представления

---
+++ ASP.NET_Core/MVC/02_METANIT_MVCTutorial/07_ViewComponent/Components/UsersListViewComponent.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _07_ViewComponent.Components;
+
+public class UsersListViewComponent : ViewComponent {
+
+    List<string> users = new List<string> { "Tom", "Tim", "Bob", "Sam" };
+
+    public IViewComponentResult Invoke() => View(users);
+}
