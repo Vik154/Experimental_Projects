52b366e8e2fb19f3d9be79e8ac38e89fd10d404f
Vik154 <Smorodin_1209@mail.ru>
2023-11-11 21:47:51 +0700

    Сортировка данных

---
+++ ASP.NET_Core/MVC/02_METANIT_MVCTutorial/10_EntityFrameworkSort/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using _10_EntityFrameworkSort.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace _10_EntityFrameworkSort.Controllers;
+
+public class HomeController : Controller {
+
+    UsersContext _db;
+    
+    public HomeController(UsersContext context) {
+        _db = context;
+
+        // добавим начальные данные для тестирования
+        if (!_db.Companies.Any()) {
+            Company oracle    = new Company { Name = "Oracle" };
+            Company google    = new Company { Name = "Google" };
+            Company microsoft = new Company { Name = "Microsoft" };
+            Company apple     = new Company { Name = "Apple" };
+
+            User user1 = new User { Name = "Олег Васильев", Company = oracle, Age = 26 };
+            User user2 = new User { Name = "Александр Овсов", Company = oracle, Age = 24 };
+            User user3 = new User { Name = "Алексей Петров", Company = microsoft, Age = 25 };
+            User user4 = new User { Name = "Иван Иванов", Company = microsoft, Age = 26 };
+            User user5 = new User { Name = "Петр Андреев", Company = microsoft, Age = 23 };
+            User user6 = new User { Name = "Василий Иванов", Company = google, Age = 23 };
+            User user7 = new User { Name = "Олег Кузнецов", Company = google, Age = 25 };
+            User user8 = new User { Name = "Андрей Петров", Company = apple, Age = 24 };
+
+            _db.Companies.AddRange(oracle, microsoft, google, apple);
+            _db.Users.AddRange(user1, user2, user3, user4, user5, user6, user7, user8);
+            _db.SaveChanges();
+        }
+    }
+    public async Task<IActionResult> Index(SortState sortOrder = SortState.NameAsc) {
+        IQueryable<User>? users = _db.Users.Include(x => x.Company);
+
+        ViewData["NameSort"] = sortOrder == SortState.NameAsc ? SortState.NameDesc : SortState.NameAsc;
+        ViewData["AgeSort"]  = sortOrder == SortState.AgeAsc ? SortState.AgeDesc : SortState.AgeAsc;
+        ViewData["CompSort"] = sortOrder == SortState.CompanyAsc ? SortState.CompanyDesc : SortState.CompanyAsc;
+
+        users = sortOrder switch {
+            SortState.NameDesc    => users.OrderByDescending(s => s.Name),
+            SortState.AgeAsc      => users.OrderBy(s => s.Age),
+            SortState.AgeDesc     => users.OrderByDescending(s => s.Age),
+            SortState.CompanyAsc  => users.OrderBy(s => s.Company!.Name),
+            SortState.CompanyDesc => users.OrderByDescending(s => s.Company!.Name),
+            _ => users.OrderBy(s => s.Name),
+        };
+        return View(await users.AsNoTracking().ToListAsync());
+    }
+}
--- ASP.NET_Core/MVC/02_METANIT_MVCTutorial/10_EntityFrameworkSort/Program.cs
+++ ASP.NET_Core/MVC/02_METANIT_MVCTutorial/10_EntityFrameworkSort/Program.cs
@@ -1,12 +1,21 @@
-﻿namespace _10_EntityFrameworkSort;
+﻿using _10_EntityFrameworkSort.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace _10_EntityFrameworkSort;
 
 
 public class Program {
     public static void Main(string[] args) {
         var builder = WebApplication.CreateBuilder(args);
+
+        string connection = "Server = (localdb)\\mssqllocaldb;Database = userstoredb;Trusted_Connection=true";
+        builder.Services.AddDbContext<UsersContext>(options => options.UseSqlServer(connection));
+
+        builder.Services.AddControllersWithViews();
+
         var app = builder.Build();
 
-        app.MapGet("/", () => "Hello World!");
+        app.MapDefaultControllerRoute();
 
         app.Run();
     }
