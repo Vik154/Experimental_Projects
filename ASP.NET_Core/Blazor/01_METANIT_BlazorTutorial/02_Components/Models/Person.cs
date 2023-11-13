8a287722d327a965a27f018f681f2572cd4ef381
Vik154 <Smorodin_1209@mail.ru>
2023-11-13 20:40:07 +0700

    Привязка моделей

---
+++ ASP.NET_Core/Blazor/01_METANIT_BlazorTutorial/02_Components/Models/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace _02_Components.Models;
+
+public class Person {
+    public string Name { get; set; } = "";
+    public int Age { get; set; }
+    public DateTime Day { get; set; } = DateTime.Now;
+    public bool Working { get; set; }
+    public string Subject { get; set; } = "";
+    public string[] Courses { get; set; } = new string[0];
+}
