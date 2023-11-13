d56a378a381c154f6393f943818fc3f0eb9c6897
Vik154 <Smorodin_1209@mail.ru>
2023-11-13 20:59:51 +0700

    Встроенные компоненты ввода

---
+++ ASP.NET_Core/Blazor/01_METANIT_BlazorTutorial/03_Forms/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace _03_Forms.Models;
+
+public class Student {
+    public string Name { get; set; } = "";
+    public int Age { get; set; }
+    public DateTime EnrollmentDate { get; set; } = DateTime.Now; // дата поступления
+    public bool HasWork { get; set; }                           // работает или нет
+    public string Subject { get; set; } = "";                   // специальность
+    public string[] Courses { get; set; } = new string[] { };   // доп. курсы
+}
