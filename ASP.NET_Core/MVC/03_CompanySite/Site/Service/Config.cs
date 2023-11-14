d3a1421e432c5e8f2482d548a1bc55742fc89b5e
Vik154 <Smorodin_1209@mail.ru>
2023-11-14 13:52:36 +0700

    Класс-обёртка соответствующая настройкам appsettings.json

---
+++ ASP.NET_Core/MVC/03_CompanySite/Site/Service/Config.cs
@@ -0,0 +1,10 @@
+﻿namespace Site.Service;
+
+/// <summary> Класс-обёртка соответствующая настройкам appsettings.json </summary>
+public class Config {
+    public static string? ConnectionString { get; set; }
+    public static string? CompanyName { get; set; }
+    public static string? CompanyPhone { get; set; }
+    public static string? CompanyPhoneShort { get; set; }
+    public static string? CompanyEmail { get; set; }
+}
