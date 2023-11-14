a6e0039465e1f81b0bc315e9de319603f21cffb3
Vik154 <Smorodin_1209@mail.ru>
2023-11-14 21:57:31 +0700

    Добавление своих стилей

--- ASP.NET_Core/MVC/04_SimpleWebSite/WebSite/Controllers/HomeController.cs
+++ ASP.NET_Core/MVC/04_SimpleWebSite/WebSite/Controllers/HomeController.cs
@@ -8,17 +8,9 @@ public class HomeController : Controller {
 
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger) {
-        _logger = logger;
-    }
+    public HomeController(ILogger<HomeController> logger) => _logger = logger;
 
-    public IActionResult Index() {
-        return View();
-    }
-
-    public IActionResult About() {
-        return View();
-    }
+    public IActionResult Index() => View();
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error() {
