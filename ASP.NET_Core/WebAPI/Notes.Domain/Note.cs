92e882f0f99bbe5c716c2a6bc3a003501f444146
Vik154 <Smorodin_1209@mail.ru>
2023-11-15 01:38:34 +0700

    Создание основной архитектуры приложения

---
+++ ASP.NET_Core/WebAPI/Notes.Domain/Note.cs
@@ -0,0 +1,11 @@
+﻿namespace Notes.Domain;
+
+/// <summary> Класс представляющий заметку </summary>
+public class Note {
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+    public string? Title { get; set; }
+    public string? Details { get; set; }
+    public DateTime CreationDate { get; set; }
+    public DateTime? EditDate { get; set; }
+}
