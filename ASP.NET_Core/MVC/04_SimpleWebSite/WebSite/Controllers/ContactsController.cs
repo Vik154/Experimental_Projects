980a335e593683fe87826bf9f60064a04c6153dc
Vik154 <Smorodin_1209@mail.ru>
2023-11-15 00:03:03 +0700

    Пример работы

--- ASP.NET_Core/MVC/04_SimpleWebSite/WebSite/Controllers/ContactsController.cs
+++ ASP.NET_Core/MVC/04_SimpleWebSite/WebSite/Controllers/ContactsController.cs
@@ -1,7 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebSite.Models;
 
 namespace WebSite.Controllers;
 
 public class ContactsController : Controller {
     public IActionResult Index() => View();
+
+    [HttpPost]
+    public IActionResult Check(Contact contact) {
+        if (ModelState.IsValid) {
+            return Redirect("/");
+        }
+        return View("Index");
+    }
 }
--- ASP.NET_Core/MVC/04_SimpleWebSite/WebSite/Models/Contact.cs
+++ ASP.NET_Core/MVC/04_SimpleWebSite/WebSite/Models/Contact.cs
@@ -6,11 +6,11 @@ public class Contact {
 
     [Display(Name = "Введите имя:")]
     [Required(ErrorMessage = "Обязательное поле")]
-    public string Name { get; set; } = "";
+    public string Name { get; set; }
 
     [Display(Name = "Введите фамилию:")]
     [Required(ErrorMessage = "Обязательное поле")]
-    public string Surname { get; set; } = "";
+    public string Surname { get; set; }
 
     [Display(Name = "Введите возраст:")]
     [Required(ErrorMessage = "Обязательное поле")]
@@ -18,9 +18,9 @@ public class Contact {
 
     [Display(Name = "Введите Email:")]
     [Required(ErrorMessage = "Обязательное поле")]
-    public string Email { get; set; } = "";
+    public string Email { get; set; }
 
     [Display(Name = "Введите сообщение:")]
     [Required(ErrorMessage = "Обязательное поле")]
-    public string Message { get; set; } = "";
+    public string Message { get; set; }
 }
