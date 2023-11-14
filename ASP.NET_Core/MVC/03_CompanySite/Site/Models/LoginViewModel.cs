a42eb5a7cc019ad7bb98b73a6521716ef83770b8
Vik154 <Smorodin_1209@mail.ru>
2023-11-14 18:00:14 +0700

    Добавление view model

---
+++ ASP.NET_Core/MVC/03_CompanySite/Site/Models/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Site.Models; 
+
+public class LoginViewModel {
+
+    [Required]
+    [Display(Name = "Login")]
+    public string? UserName { get; set; }
+
+    [Required]
+    [UIHint("password")]
+    [Display(Name = "Пароль")]
+    public string? Password { get; set; }
+
+    [Display(Name = "Запомнить меня?")]
+    public bool RememberMe { get; set; }
+}
