7a484bf4f1c48ba3d743f1f348df9242d4c5cebb
Vik154 <Smorodin_1209@mail.ru>
2023-11-14 16:57:18 +0700

    Добавление миграций настройка SQL Server

---
+++ ASP.NET_Core/MVC/03_CompanySite/Site/Migrations/20231114094439__Console.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Site.Migrations
+{
+    /// <inheritdoc />
+    public partial class _Console : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "3b62472e-4f66-49fa-a20f-e7685b9565d8",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "56c7c067-7822-4954-9e23-97d72423d276", "AQAAAAIAAYagAAAAEEKkNwjnGKRdRcT0Zm5sorYoNxyeBDuL1zE4xf87vUR9Qy3FDMrdJGbcgId33y7G/w==" });
+
+            migrationBuilder.UpdateData(
+                table: "TextFields",
+                keyColumn: "Id",
+                keyValue: new Guid("4aa76a4c-c59d-409a-84c1-06e6487a137a"),
+                column: "DateAdded",
+                value: new DateTime(2023, 11, 14, 9, 44, 39, 383, DateTimeKind.Utc).AddTicks(7847));
+
+            migrationBuilder.UpdateData(
+                table: "TextFields",
+                keyColumn: "Id",
+                keyValue: new Guid("63dc8fa6-07ae-4391-8916-e057f71239ce"),
+                column: "DateAdded",
+                value: new DateTime(2023, 11, 14, 9, 44, 39, 383, DateTimeKind.Utc).AddTicks(7662));
+
+            migrationBuilder.UpdateData(
+                table: "TextFields",
+                keyColumn: "Id",
+                keyValue: new Guid("70bf165a-700a-4156-91c0-e83fce0a277f"),
+                column: "DateAdded",
+                value: new DateTime(2023, 11, 14, 9, 44, 39, 383, DateTimeKind.Utc).AddTicks(7781));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "3b62472e-4f66-49fa-a20f-e7685b9565d8",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "03a93ed9-49e5-4a4c-8248-ef190dcd81a7", "AQAAAAIAAYagAAAAENK5z3sTkZ9zCt8s0HTK5LRqiXBeAOisjpC49FFLxB3cr0IosVrkjCZ6K0WLfgW/3Q==" });
+
+            migrationBuilder.UpdateData(
+                table: "TextFields",
+                keyColumn: "Id",
+                keyValue: new Guid("4aa76a4c-c59d-409a-84c1-06e6487a137a"),
+                column: "DateAdded",
+                value: new DateTime(2023, 11, 14, 9, 29, 42, 800, DateTimeKind.Utc).AddTicks(6891));
+
+            migrationBuilder.UpdateData(
+                table: "TextFields",
+                keyColumn: "Id",
+                keyValue: new Guid("63dc8fa6-07ae-4391-8916-e057f71239ce"),
+                column: "DateAdded",
+                value: new DateTime(2023, 11, 14, 9, 29, 42, 800, DateTimeKind.Utc).AddTicks(6804));
+
+            migrationBuilder.UpdateData(
+                table: "TextFields",
+                keyColumn: "Id",
+                keyValue: new Guid("70bf165a-700a-4156-91c0-e83fce0a277f"),
+                column: "DateAdded",
+                value: new DateTime(2023, 11, 14, 9, 29, 42, 800, DateTimeKind.Utc).AddTicks(6868));
+        }
+    }
+}
--- ASP.NET_Core/MVC/03_CompanySite/Site/Program.cs
+++ ASP.NET_Core/MVC/03_CompanySite/Site/Program.cs
@@ -1,4 +1,9 @@
-﻿using Site.Service;
+﻿using Site.Domain.Repositories.Abstract;
+using Site.Domain.Repositories.EntityFramework;
+using Site.Domain;
+using Site.Service;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace Site;
 
@@ -10,15 +15,64 @@ public class Program {
         // Подключение конфига appsettings.json секции "Project"
         builder.Configuration.Bind("Project", new Config());
 
-        // Использование MVC
+		// Подключаем нужный функционал приложения в качестве сервисов
+		builder.Services.AddTransient<ITextFieldsRepository, EFTextFieldsRepository>();
+		builder.Services.AddTransient<IServiceItemsRepository, EFServiceItemsRepository>();
+		builder.Services.AddTransient<DataManager>();
+
+		// Подключаем контекст БД
+		builder.Services.AddDbContext<AppDbContext>((context) => context.UseSqlServer(Config.ConnectionString));
+
+		// Настраиваем identity систему
+		builder.Services.AddIdentity<IdentityUser, IdentityRole>((opts) => {
+			opts.User.RequireUniqueEmail = true;
+			opts.Password.RequiredLength = 6;
+			opts.Password.RequireNonAlphanumeric = false;
+			opts.Password.RequireLowercase = false;
+			opts.Password.RequireUppercase = false;
+			opts.Password.RequireDigit = false;
+		}).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
+
+		// Настраиваем authentication cooke
+		builder.Services.ConfigureApplicationCookie((options) => {
+			options.Cookie.Name = "myCompanyAuth";
+			options.Cookie.HttpOnly = true;
+			options.LoginPath = "/account/login";
+			options.AccessDeniedPath = "/account/accessdenied";
+			options.SlidingExpiration = true;
+		});
+
+		// Настройка политики авторизации для Admin area
+		builder.Services.AddAuthorization((x) => {
+			x.AddPolicy("AdminArea", (policy) => { policy.RequireRole("admin"); });
+		});
+
+		// Добавляем поддержку контроллеров и представлений (MVC)
 		builder.Services.AddControllersWithViews();
+		//builder.Services.AddControllersWithViews((x) => {
+		//	x.Conventions.Add(new AdminAreaAuthorization("Admin", "AdminArea"));
+		//})
 
 		var app = builder.Build();
 
-        app.UseRouting();
-        app.UseStaticFiles();
+		// Порядок регистрации middleware очень важен
+		// Подробная информация об ошибках в процессе разработки
+		if (app.Environment.IsDevelopment()) app.UseDeveloperExceptionPage();
 
-        app.MapControllerRoute("def", "{controller=Home}/{action=Index}/{id?}");
+		// Подключение поддержки статичных файлов в приложении (css, js)
+		app.UseStaticFiles();
+
+		// Подключение системы маршрутизации
+		app.UseRouting();
+
+		// Подключение аутентификации и авторизации
+		app.UseCookiePolicy();
+		app.UseAuthentication();
+		app.UseAuthorization();
+
+		// Регистрация нужных маршрутов
+        app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+		app.MapControllerRoute("admin", "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
         app.Run();
     }
