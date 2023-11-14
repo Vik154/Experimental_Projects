f301942d7b878a0f29a5b1787de8df9e55670ade
Vik154 <Smorodin_1209@mail.ru>
2023-11-14 15:10:17 +0700

    Добавленние доменного уровня и моделей

---
+++ ASP.NET_Core/MVC/03_CompanySite/Site/Domain/Entities/ServiceItem.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Site.Domain.Entities;
+
+/// <summary> Класс представляющий услугу на сайте </summary>
+public class ServiceItem : EntityBase {
+
+    [Required(ErrorMessage = "Заполните название услуги")]
+    [Display(Name = "Название услуги")]
+    public override string? Title { get; set; }
+
+    [Display(Name = "Краткое описание услуги")]
+    public override string? Subtitle { get; set; }
+
+    [Display(Name = "Полное описание услуги")]
+    public override string? Text { get; set; }
+}
--- ASP.NET_Core/MVC/03_CompanySite/Site/Program.cs
+++ ASP.NET_Core/MVC/03_CompanySite/Site/Program.cs
@@ -7,8 +7,10 @@ public class Program {
     public static void Main(string[] args) {
         var builder = WebApplication.CreateBuilder(args);
 
+        // Подключение конфига appsettings.json секции "Project"
         builder.Configuration.Bind("Project", new Config());
 
+        // Использование MVC
 		builder.Services.AddControllersWithViews();
 
 		var app = builder.Build();
