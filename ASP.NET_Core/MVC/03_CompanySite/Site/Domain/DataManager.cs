507bd9e94a9e36863e434cc21009bfd0f2723586
Vik154 <Smorodin_1209@mail.ru>
2023-11-14 16:10:49 +0700

    Класс-помощник, точка входа для DataBase контекста

---
+++ ASP.NET_Core/MVC/03_CompanySite/Site/Domain/DataManager.cs
@@ -0,0 +1,15 @@
+﻿using Site.Domain.Repositories.Abstract;
+
+namespace Site.Domain;
+
+/// <summary> Класс-помощник, точка входа для DataBase контекста </summary>
+public class DataManager {
+
+    public ITextFieldsRepository TextFields { get; set; }
+    public IServiceItemsRepository ServiceItems { get; set; }
+
+    public DataManager(ITextFieldsRepository textFields, IServiceItemsRepository serviceItems) {
+        TextFields = textFields;
+        ServiceItems = serviceItems;
+    }
+}
