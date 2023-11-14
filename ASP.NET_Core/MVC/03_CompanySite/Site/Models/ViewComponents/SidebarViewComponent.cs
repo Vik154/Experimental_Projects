50d56de5e3d787fbf3518380b9e69e3d2cfe43d4
Vik154 <Smorodin_1209@mail.ru>
2023-11-14 19:35:57 +0700

    Добавление компонента sidebar

---
+++ ASP.NET_Core/MVC/03_CompanySite/Site/Models/ViewComponents/SidebarViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Site.Domain;
+
+namespace Site.Models.ViewComponents;
+
+public class SidebarViewComponent : ViewComponent {
+    private readonly DataManager dataManager;
+
+    public SidebarViewComponent(DataManager dataManager) {
+        this.dataManager = dataManager;
+    }
+
+    public Task<IViewComponentResult> InvokeAsync() {
+        return Task.FromResult((IViewComponentResult)View("Default", dataManager.ServiceItems.GetServiceItems()));
+    }
+}
