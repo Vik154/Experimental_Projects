a2bc5e2872124cf021d3e10af9ca88fd99930ce9
Vik154 <Smorodin_1209@mail.ru>
2023-11-14 13:03:50 +0700

    Настройка и связывание файлов

--- ASP.NET_Core/MVC/03_CompanySite/Site/Program.cs
+++ ASP.NET_Core/MVC/03_CompanySite/Site/Program.cs
@@ -3,9 +3,15 @@
 public class Program {
     public static void Main(string[] args) {
         var builder = WebApplication.CreateBuilder(args);
-        var app = builder.Build();
 
-        app.MapGet("/", () => "Hello World!");
+		builder.Services.AddControllersWithViews();
+
+		var app = builder.Build();
+
+        app.UseRouting();
+        app.UseStaticFiles();
+
+        app.MapControllerRoute("def", "{controller=Home}/{action=Index}/{id?}");
 
         app.Run();
     }
