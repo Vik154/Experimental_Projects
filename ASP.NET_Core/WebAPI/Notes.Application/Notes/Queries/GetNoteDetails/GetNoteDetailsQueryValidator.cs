62cd62cee63c88abbc78f1148addb9e19ead7457
Vik154 <Smorodin_1209@mail.ru>
2023-11-17 22:22:11 +0700

    Добавление правил валидации

---
+++ ASP.NET_Core/WebAPI/Notes.Application/Notes/Queries/GetNoteDetails/GetNoteDetailsQueryValidator.cs
@@ -0,0 +1,10 @@
+﻿using FluentValidation;
+
+namespace Notes.Application.Notes.Queries.GetNoteDetails;
+
+public class GetNoteDetailsQueryValidator : AbstractValidator<GetNoteDetailsQuery> {
+    public GetNoteDetailsQueryValidator() {
+        RuleFor(x => x.UserId).NotEqual(Guid.Empty);
+        RuleFor(x => x.Id).NotEqual(Guid.Empty);
+    }
+}
