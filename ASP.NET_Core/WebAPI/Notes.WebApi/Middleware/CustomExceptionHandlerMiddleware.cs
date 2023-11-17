e5187d743323b3396e9438a2f55b51fe641ed893
Vik154 <Smorodin_1209@mail.ru>
2023-11-17 23:07:54 +0700

    Кастомный конвейер

---
+++ ASP.NET_Core/WebAPI/Notes.WebApi/Middleware/CustomExceptionHandlerMiddleware.cs
@@ -0,0 +1,47 @@
+﻿using Notes.Application.Common.Exceptions;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Text.Json;
+
+namespace Notes.WebApi.Middleware;
+
+public class CustomExceptionHandlerMiddleware {
+    private readonly RequestDelegate _next;
+
+    public CustomExceptionHandlerMiddleware(RequestDelegate next) {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context) {
+		try {
+            await _next(context);
+		}
+		catch (Exception exp) {
+            await HandleExceptionAsync(context, exp);
+		}
+    }
+
+    private Task HandleExceptionAsync(HttpContext context, Exception exp) {
+        var code = HttpStatusCode.InternalServerError;
+        var result = string.Empty;
+
+        switch (exp) {
+            case ValidationException validationException:
+                code = HttpStatusCode.BadRequest;
+                result = JsonSerializer.Serialize(validationException);
+                break;
+            case NotFoundException:
+                code = HttpStatusCode.NotFound;
+                break;
+        }
+
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)code;
+
+        if (result == string.Empty) {
+            result = JsonSerializer.Serialize(new {error = exp.Message});
+        }
+
+        return context.Response.WriteAsync(result);
+    }
+}
