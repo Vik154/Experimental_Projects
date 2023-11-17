866b464aa72bae8eb1a184f4460dd2f4d2aec537
Vik154 <Smorodin_1209@mail.ru>
2023-11-17 19:14:34 +0700

    Внедрение зависимостей

---
+++ ASP.NET_Core/WebAPI/Notes.Application/DependencyInjection.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Notes.Application;
+
+public static class DependencyInjection {
+    public static IServiceCollection AddApplication(this IServiceCollection services) {
+        return services;
+    }
+}
