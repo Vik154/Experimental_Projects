8cf13a64c251a139206a9262d7485712f62713fc
Vik154 <Smorodin_1209@mail.ru>
2023-11-19 06:40:17 +0700

    Добавление данных

--- ASP.NET_Core/MVC/05_RunGroopApplication/RunGroopWebApp/Program.cs
+++ ASP.NET_Core/MVC/05_RunGroopApplication/RunGroopWebApp/Program.cs
@@ -16,6 +16,10 @@ public class Program {
 
         var app = builder.Build();
 
+        if (args.Length == 1 && args[0].ToLower() == "seeddata") {
+            Seed.SeedData(app);
+        }
+
         // Configure the HTTP request pipeline.
         if (!app.Environment.IsDevelopment()) {
             app.UseExceptionHandler("/Home/Error");
