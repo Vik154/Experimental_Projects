cd66f880e8ac0c58804e5c87f461c3ddd9acd7c8
Vik154 <Smorodin_1209@mail.ru>
2023-11-19 04:33:15 +0700

    Веб-приложение для регистрации бега

---
+++ ASP.NET_Core/MVC/05_RunGroopApplication/RunGroopWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace RunGroopWebApp.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
