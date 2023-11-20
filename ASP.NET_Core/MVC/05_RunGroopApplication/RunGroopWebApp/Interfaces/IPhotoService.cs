02c489a11cacc1e230b67c2c55000760309bff4b
Vik154 <Smorodin_1209@mail.ru>
2023-11-20 16:58:04 +0700

    DTO хранение промежуточных состояний

---
+++ ASP.NET_Core/MVC/05_RunGroopApplication/RunGroopWebApp/Interfaces/IPhotoService.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RunGroopWebApp.Interfaces;
+
+public interface IPhotoService {
+    Task<FileResult> AddPhotoAsync(IFormFile file);
+    Task<FileResult> DeletePhotoAsync(string publicUrl);
+}
---
+++ ASP.NET_Core/MVC/05_RunGroopApplication/RunGroopWebApp/Services/PhotoService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RunGroopWebApp.Interfaces;
+
+namespace RunGroopWebApp.Services;
+
+public class PhotoService : IPhotoService {
+
+    /* Тут можно логику запилить из серии загрузки файлов из облака,
+     * но надо API смотреть какие лучше подходят и ключи с токенами 
+     * хранить где-то отдельно, ну точно в репозиториях на гите */
+
+    public Task<FileResult> AddPhotoAsync(IFormFile file) {
+        throw new NotImplementedException();
+    }
+
+    public Task<FileResult> DeletePhotoAsync(string publicUrl) {
+        throw new NotImplementedException();
+    }
+}
