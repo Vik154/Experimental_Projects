6686fac327a6bcdf7deac6f20384a0de251f75cf
Vik154 <Smorodin_1209@mail.ru>
2023-11-26 06:05:46 +0700

    Удаление записи из БД

--- ASP.NET_Core/MVC/06_CoreCollection/01_CRUD_Operations/Controllers/TransactionsController.cs
+++ ASP.NET_Core/MVC/06_CoreCollection/01_CRUD_Operations/Controllers/TransactionsController.cs
@@ -11,25 +11,8 @@ public class TransactionsController : Controller
     public TransactionsController(TransactionDbContext context) => _context = context;
 
     // GET: Transactions
-    public async Task<IActionResult> Index()
-    {
+    public async Task<IActionResult> Index() {
         return View(await _context.Transactions.ToListAsync());
-    }
-
-    // GET: Transactions/Details/5
-    public async Task<IActionResult> Details(int? id) {
-        if (id == null) {
-            return NotFound();
-        }
-
-        var transaction = await _context.Transactions
-            .FirstOrDefaultAsync(m => m.TransactionId == id);
-        
-        if (transaction == null) {
-            return NotFound();
-        }
-
-        return View(transaction);
     }
 
     // GET: Transactions/AddOrEdit
@@ -71,10 +54,5 @@ public class TransactionsController : Controller
 
         await _context.SaveChangesAsync();
         return RedirectToAction(nameof(Index));
-    }
-
-    private bool TransactionExists(int id)
-    {
-        return _context.Transactions.Any(e => e.TransactionId == id);
     }
 }
