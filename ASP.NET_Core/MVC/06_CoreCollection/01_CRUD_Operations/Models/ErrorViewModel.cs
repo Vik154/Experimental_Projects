ee0dc04ffc38c62af462b575dc4a3f374d62c705
Vik154 <Smorodin_1209@mail.ru>
2023-11-26 02:57:12 +0700

    Пример crud операций

---
+++ ASP.NET_Core/MVC/06_CoreCollection/01_CRUD_Operations/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace _01_CRUD_Operations.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
