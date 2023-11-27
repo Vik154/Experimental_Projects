672231fd24f991812684196bee08d37478d68110
Vik154 <Smorodin_1209@mail.ru>
2023-11-27 19:06:56 +0700

    Добавление миграций

--- ASP.NET_Core/MVC/06_CoreCollection/02_ExpenseTracker/Program.cs
+++ ASP.NET_Core/MVC/06_CoreCollection/02_ExpenseTracker/Program.cs
@@ -12,7 +12,7 @@ public class Program {
 
         // Добавление контекста БД
         builder.Services.AddDbContext<ApplicationDbContext>(options => {
-            options.UseSqlServer(builder.Configuration.GetConnectionString(""));
+            options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionSQL"));
         });
 
         var app = builder.Build();
@@ -20,7 +20,6 @@ public class Program {
         // Configure the HTTP request pipeline.
         if (!app.Environment.IsDevelopment()) {
             app.UseExceptionHandler("/Home/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
         }
 
