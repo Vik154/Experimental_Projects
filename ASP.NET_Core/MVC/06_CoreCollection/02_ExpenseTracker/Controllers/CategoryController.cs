633f51e3799b8b845763f929571fb8cfbc05a715
Vik154 <Smorodin_1209@mail.ru>
2023-11-27 21:23:53 +0700

    Вёрстка формы добавления / редактирования операций

--- ASP.NET_Core/MVC/06_CoreCollection/02_ExpenseTracker/Controllers/CategoryController.cs
+++ ASP.NET_Core/MVC/06_CoreCollection/02_ExpenseTracker/Controllers/CategoryController.cs
@@ -37,42 +37,13 @@ public class CategoryController : Controller
         return View(category);
     }
 
-    // GET: Category/Create
-    public IActionResult Create()
-    {
-        return View(new Category());
-    }
 
-    // POST: Category/Create
-    // To protect from overposting attacks, enable the specific properties you want to bind to.
-    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Create([Bind("CategoryId,Title,Icon,Type")] Category category)
-    {
-        if (ModelState.IsValid)
-        {
-            _context.Add(category);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-        return View(category);
-    }
-
-    // GET: Category/Edit/5
-    public async Task<IActionResult> Edit(int? id)
-    {
-        if (id == null)
-        {
-            return NotFound();
-        }
-
-        var category = await _context.Categories.FindAsync(id);
-        if (category == null)
-        {
-            return NotFound();
-        }
-        return View(category);
+    // GET: Category/AddOrEdit/id
+    public async Task<IActionResult> AddOrEdit(int id = 0) {
+        if (id == 0)
+            return View(new Category());
+        else
+            return View(_context.Categories.Find(id));
     }
 
     // POST: Category/Edit/5
@@ -80,7 +51,7 @@ public class CategoryController : Controller
     // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Edit(int id, [Bind("CategoryId,Title,Icon,Type")] Category category)
+    public async Task<IActionResult> AddOrEdit(int id, [Bind("CategoryId,Title,Icon,Type")] Category category)
     {
         if (id != category.CategoryId)
         {
