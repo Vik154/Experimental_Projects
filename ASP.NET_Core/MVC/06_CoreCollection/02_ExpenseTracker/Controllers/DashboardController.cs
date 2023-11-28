900b7b1b8244f36f1c7c72f6be75b310e21e088e
Vik154 <Smorodin_1209@mail.ru>
2023-11-28 17:07:15 +0700

    Добавление боковой панели управления

---
+++ ASP.NET_Core/MVC/06_CoreCollection/02_ExpenseTracker/Controllers/DashboardController.cs
@@ -0,0 +1,24 @@
+﻿using _02_ExpenseTracker.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace _02_ExpenseTracker.Controllers;
+
+public class DashboardController : Controller {
+
+    private readonly ApplicationDbContext _context;
+
+    public DashboardController(ApplicationDbContext context) => _context = context;
+
+    public async Task<IActionResult> Index() {
+        DateTime StartDate = DateTime.Today.AddDays(-6);
+        DateTime EndDate = DateTime.Today;
+
+        List<Transaction> SelectedTransactions = await _context.Transactions
+            .Include(x => x.Category)
+            .Where(y => y.Date >= StartDate && y.Date <= EndDate)
+            .ToListAsync();
+
+        return View();
+    }
+}
